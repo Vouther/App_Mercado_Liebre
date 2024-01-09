bc2dcd9d07a183f8100922615838ba4a733c0abb
Vouther <lu_neji_shakugan09@hotmail.com>
2024-01-09 20:05:57 -0300

    Configuracion puerto

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path')
 
 const app = express();
-const port = 3005
+const port = process.env.PORT || 3001;
 
 //config
 app.use(express.static(path.join(__dirname, 'public',)))
